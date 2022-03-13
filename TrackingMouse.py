7e6b0e3463b435b683c4001d043b02d1cfc02a3d
yunost <yunost@yunost.localhost>
2022-03-13 23:48:35 +0300

    Trackin a mouse cursor

---
+++ TrackingMouse.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+
+
+# Create a black image
+img = np.zeros((800, 800, 3), np.uint8)
+
+# Initialize the Kalman filter
+
+# The first parameter is the number of variables 
+# tracked (or predicted) by the Kalman filter
+# We have 4: x, y, x-velocity, y-velocity
+# The second parameter is the number of variables provided 
+# to the Kalman filter as measurements. We have 2D
+kalman = cv2.KalmanFilter(4, 2)
+# We also initialize several matrices that describe 
+# the relationships among all these variables.
+kalman.measurementMatrix = np.array(
+        [[1, 0, 0, 0],
+         [0, 1, 0, 0]], np.float32)
+kalman.transitionMatrix = np.array(
+        [[1, 0, 1, 0],
+         [0, 1, 0, 1],
+         [0, 0, 1, 0],
+         [0, 0, 0, 1]], np.float32)
+kalman.processNoiseCov = np.eye(4, dtype=np.float32) * 0.03
+
+last_measurement = None
+last_prediction = None
+
+# callback function implementation
+def on_mouse_moved(event, x, y, flags, param):
+    global img, kalman, last_measurement, last_prediction
+
+    measurement = np.array([[x], [y]], np.float32)
+    if last_measurement is None:
+        # This is the first measurement.
+        # Update the Kalman filter's state to match the measurement
+        kalman.statePre = np.array(
+                [[x], [y], [0], [0]], np.float32)
+        kalman.statePost = np.array(
+                [[x], [y], [0], [0]], np.float32)
+        prediction = measurement
+    else:
+        kalman.correct(measurement)
+        prediction = kalman.predict() # Gets a reference, not a copy
+        # Trace the path of the measurement in green.
+        cv2.line(img, (int(last_measurement[0]), int(last_measurement[1])),
+                (int(measurement[0]), int(measurement[1])), (0,255,0))
+        # Trece the path of the prediction in red.
+        cv2.line(img, (int(last_prediction[0]), int(last_prediction[1])),
+                (int(prediction[0]), int(prediction[1])), (0,0,255))
+    last_prediction = prediction.copy()
+    last_measurement = measurement
+
+cv2.namedWindow('kalman_tracker')
+cv2.setMouseCallback('kalman_tracker', on_mouse_moved)
+
+while True:
+    cv2.imshow('kalman_tracker', img)
+    k = cv2.waitKey(1)
+    if k == 27: # Escape
+        cv2.imwrite('kalman.png', img)
+        break
